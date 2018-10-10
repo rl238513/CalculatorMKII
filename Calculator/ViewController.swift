b6d9a5c0a72be781d7a16646e67352812da6f308
EDUARDO MENDOZA <rl238513@rlas-macair-36.rlas-116.org>
2018-10-10 11:05:39 -0500

    debug and repeat the process

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,17 +12,47 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+    }
         
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool
+        {
+            textField.resignFirstResponder()
+            return true
+        }
         
+        @IBAction func additionButton(_ sender: UIButton)
+        {
+           firstNumber.resignFirstResponder()
+            secondNumber.resignFirstResponder()
+
+            let number1 = Int(firstNumber.text!)
+            let number2 = Int(secondNumber.text!)
+           
+            
+            
+            /***************************************************
+             * Start Your Code Here For MVP
+             ***************************************************/
+             let equalTo = (number1 == number2)
+           
+            
+            
+            //var equalTo = Bool()
+            //if number1 == number2 { equalTo = true }
+            //else { equalTo = false }
+            
         
-        
-        
+            /***************************************************
+             * End Your Code Here For MVP
+             ***************************************************/
+            
+            resultLabel.text = "evaluates to \(equalTo)"
+           
         
         
     }
     
-    
+    @IBOutlet weak var pictures: UIImageView!
     
     
     @IBAction func divisionButton(_ sender: Any) {
@@ -34,14 +64,17 @@ class ViewController: UIViewController {
     @IBAction func subtractionButton(_ sender: Any) {
     }
     
-    @IBAction func additionButton(_ sender: Any) {
-    }
+    //@IBAction func additionButton(_ sender: Any) {
+    //}
+    
+    @IBOutlet weak var resultLabel: UILabel!
     
     
+    @IBOutlet weak var secondNumber: UITextField!
     
     
-    
-    
+    @IBOutlet weak var firstNumber: UITextField!
     
 }
+
 
