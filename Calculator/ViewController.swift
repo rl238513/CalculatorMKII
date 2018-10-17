e54179a41b9775414f35ddd400327fe55e1efbfe
EDUARDO MENDOZA <rl238513@rlas-macair-36.rlas-116.org>
2018-10-17 11:05:42 -0500

    done

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,14 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var secondNumber: UITextField!
     @IBOutlet weak var firstNumber: UITextField!
-    @IBOutlet weak var gif: UIImageView!
+    @IBOutlet weak var creator: UILabel!
+    var myName:String = "Eduardo Mendoza"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pictures.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleBottomMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleHeight.rawValue) | UInt8(UIViewAutoresizing.flexibleRightMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleLeftMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleTopMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleWidth.rawValue)))
         pictures.contentMode = UIViewContentMode.scaleAspectFill
+        
+        creator.text = favoriteClassName
+        
+        
     }
-    
+   
     @IBAction func additionButton(_ sender: UIButton)
     {
         firstNumber.resignFirstResponder()
@@ -42,7 +48,7 @@ class ViewController: UIViewController {
             }
         }
         else {
-            let alert = UIAlertController(title: "empty textfield", message: "blank text field", preferredStyle: .alert)
+            let alert = UIAlertController(title: "empty textfield", message: "good job dumb dumb want some gum gum", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
@@ -69,7 +75,7 @@ class ViewController: UIViewController {
             }
         }
         else {
-            let alert = UIAlertController(title: "empty textfield", message: "blank text field", preferredStyle: .alert)
+            let alert = UIAlertController(title: "empty textfield", message: "good job dumb dumb want some gum gum", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
@@ -96,7 +102,7 @@ class ViewController: UIViewController {
             }
         }
         else {
-            let alert = UIAlertController(title: "empty textfield", message: "blank text field", preferredStyle: .alert)
+            let alert = UIAlertController(title: "empty textfield", message: "good job dumb dumb want some gum gum", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
@@ -123,10 +129,11 @@ class ViewController: UIViewController {
             }
         }
         else {
-            let alert = UIAlertController(title: "empty textfield", message: "blank text field", preferredStyle: .alert)
+            let alert = UIAlertController(title: "empty textfield", message: "good job dumb dumb want some gum gum", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
 }
 
 
+}
